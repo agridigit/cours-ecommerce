fdd485c5ca7bd6aaf3fbadb8c17d5b99b4918996
amine <bouslamaamine@gmail.com>
2021-11-09 16:14:39 +0100

    Add Doctrine

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,23 +3,32 @@
 namespace App\Controller;
 
 
-use App\Taxes\Detector;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class HelloController
+class HelloController extends AbstractController
 {
     /**
      * @param Request $request
      * @param $name
      * @return Response
-     * @Route ("/hello/{name}", name="hello")
+     * @Route ("/hello/{name?Word}", name="hello")
      */
-    public function hello($name = "Word"): Response
+    public function hello($name, Environment $environment): Response
     {
-
-        return new Response("Hello $name");
+        $html = $environment->render('hello.html.twig', [
+            'name' => $name,
+            'trainer' => [
+               'firstName' => 'Amine',
+                'LastName' => 'Bouslama',
+                'age' => 44
+            ]
+        ]);
+        return new Response($html);
     }
 
 }
